3718144417f32be93dad5d381ed54f4afabd6b43
KseniaYudina <uksenia359@gmail.com>
2025-01-20 22:52:18 +0300

    Initial commit

---
+++ src/test/java/RequestsTest.java
@@ -0,0 +1,26 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RequestsTest {
+   @Test
+
+    public void Test1(){
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("data"))
+        ;
+    }
+
+}
