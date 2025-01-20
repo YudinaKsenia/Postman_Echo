f9584bdcc6ebcedf54d1377abc501eb2cb4373e6
KseniaYudina <uksenia359@gmail.com>
2025-01-20 22:56:38 +0300

    add README.md modified test

--- src/test/java/RequestsTest.java
+++ src/test/java/RequestsTest.java
@@ -19,7 +19,7 @@ public class RequestsTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("data"))
+                .body("data", equalTo("some data"))
         ;
     }
 
